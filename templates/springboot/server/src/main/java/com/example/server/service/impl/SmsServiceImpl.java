b173a7d6507513b07dd033cc3322f9761cd0fd28
清净 <qingjing666666@126.com>
2026-02-26 22:12:12 +0800

    通过配置可修改验证码有效期

--- templates/springboot/server/src/main/java/com/example/server/service/impl/SmsServiceImpl.java
+++ templates/springboot/server/src/main/java/com/example/server/service/impl/SmsServiceImpl.java
@@ -27,6 +27,8 @@ public class SmsServiceImpl implements SmsService {
     private final Client phoneAuthClient;
     private final StringRedisTemplate redisTemplate;
     private final JavaMailSender mailSender;
+    @Value("${example.verify-code.ttl}")
+    private Long verifyCodeTtl;
     @Value("${spring.mail.username}")
     private String from;
 
@@ -38,9 +40,9 @@ public class SmsServiceImpl implements SmsService {
                 .setPhoneNumber(phone)// 短信接收方手机号
                 .setSignName("速通互联验证码")// 短信签名名称
                 .setTemplateCode("100001")// 短信模板编码
-                .setTemplateParam("{\"code\":\"##code##\",\"min\":\"5\"}")// 短信模板参数, "##code##" 为阿里云服务生成的验证码
+                .setTemplateParam("{\"code\":\"##code##\",\"min\":\"" + verifyCodeTtl + "\"}")// 短信模板参数, "##code##" 为阿里云服务生成的验证码
                 .setCodeLength(6L)// 验证码长度，默认 4位
-                .setValidTime(300L)// 验证码有效期，默认 300 秒 (5分钟)
+                .setValidTime(verifyCodeTtl * 60L)// 验证码有效期，默认 300 秒 (5分钟)
                 .setDuplicatePolicy(1L)// 重复校验策略，1：覆盖处理（默认），即旧验证码会失效掉。2：保留，即多个验证码都是在有效期内都可以校验通过。
                 .setInterval(60L)// 重复校验时间间隔，即多久间隔可以发送一次验证码，用于频控，默认 60 秒。
                 .setCodeType(1L)// 验证码类型, 1 纯数字（默认）。
@@ -53,7 +55,7 @@ public class SmsServiceImpl implements SmsService {
             phoneAuthClient.sendSmsVerifyCode(request);
             // 单号码限频：验证码：最多支持1条/分钟，5条/小时，10条/天。
             // 发送记录查询 https://dypns.console.aliyun.com/smsServiceRecord
-            log.info("发送短信验证码成功，手机号：{}", phone);
+            log.info("发送短信验证码成功，手机号：{}，有效期：{}分钟", phone, verifyCodeTtl);
         } catch (Exception e) {
             throw new BizException(e.getMessage());
         }
@@ -80,18 +82,18 @@ public class SmsServiceImpl implements SmsService {
     public void sendEmailVerifyCode(String email) {
         // 生成6位随机数字验证码
         String code = RandomUtil.randomNumbers(6);
-        // 添加到缓存（key：邮箱，value：验证码，过期时间5分钟）
+        // 添加到缓存（key：邮箱，value：验证码，过期时间 verifyCodeTtl 分钟）
         String key = RedisConstant.KeyPrefix.USER_EMAIL_KEY_PREFIX + email;
-        redisTemplate.opsForValue().set(key, code, 5, TimeUnit.MINUTES);
+        redisTemplate.opsForValue().set(key, code, verifyCodeTtl, TimeUnit.MINUTES);
         // 准备邮件
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(from);
         message.setTo(email);
         message.setSubject("验证码");  // 邮件主题
-        message.setText(String.format("您的验证码为%s。尊敬的客户，以上验证码%s分钟内有效，请注意保密，切勿告知他人。", code, 5));  // 邮件内容
+        message.setText(String.format("您的验证码为%s。尊敬的客户，以上验证码%s分钟内有效，请注意保密，切勿告知他人。", code, verifyCodeTtl));  // 邮件内容
         // 发送邮箱验证码
         mailSender.send(message);
-        log.info("发送邮箱验证码成功，邮箱：{}，验证码：{}", email, code);
+        log.info("发送邮箱验证码成功，邮箱：{}，验证码：{}，有效期：{}分钟", email, code, verifyCodeTtl);
     }
 
     @Override
